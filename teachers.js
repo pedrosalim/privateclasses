8fe89023c0cc5e601713d4196639805875b7e19a
Pedro <pedrof.salim@gmail.com>
2020-06-14 19:57:26 -0300

    js

--- teachers.js
+++ teachers.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const data = require('./data.json')
-const { age, graduation } = require('./utils')
+const { age, graduation, date } = require('./utils')
 
 exports.show =  function(req, res) {
     const { id } = req.params
@@ -59,4 +59,21 @@ exports.post = function(req, res) {
     })
 
     //return res.send(req.body)
+}
+
+exports.edit = function(req, res) {
+    const { id } = req.params
+
+    const foundTeacher = data.teachers.find(function(teacher) {
+        return teacher.id == id
+    })
+
+    if (!foundTeacher) return res.send("Teacher not found!")
+
+    const teacher = {
+        ...foundTeacher,
+        birth: date(foundTeacher.birth)
+    }
+
+    return res.render('instructors/edit', { teacher })
 }
