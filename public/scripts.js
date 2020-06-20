b3e7ce5de37623ae699821e8618bc595cb0ce3c2
Pedro <pedrof.salim@gmail.com>
2020-06-20 00:19:25 -0300

    script

--- public/scripts.js
+++ public/scripts.js
@@ -1,5 +1,5 @@
 const currentPage = location.pathname
-const menuItems = document.querySelectorAll("header .links a")]
+const menuItems = document.querySelectorAll("header .links a")
 
 for (item of menuItems) {
     if (currentPage.includes(item.getAttribute("href"))) {
